91d82b915181341895fca03b4809514557cf9b3c
Your Name <malikjawadamin32@gmail.com>
2024-05-06 23:02:21 +0500

    created number guessing game .

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+// console.log(randomNumber);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Please guess a number between 1-10:",
+    }
+]);
+if(answer.userGuessNumber === randomNumber){
+    console.log("Congratulations! you guess a right number.");
+}
+else{
+    console.log("you guesss a wrong number.");
+}
